c49eee8b2eba281fd2d5e971ed787b99d65b3e17
ahmedkaafi <ahmedkaafi.tech@gmail.com>
2025-12-03 10:46:17 +0300

    exercise4

---
+++ exercise4.js
@@ -0,0 +1,16 @@
+function addNumbers(num1, num2) {
+  console.log(num1 + num2);
+}
+addNumbers(5,10);
+addNumbers(21, 7);
+addNumbers(47, 10);
+addNumbers(500, 113);
+
+
+const add = function(num1, num2) {
+  return num1 + num2;
+}
+console.log(add(5,10));
+console.log(add(21, 7));
+console.log(add(47, 10));
+console.log(add(500, 113));
