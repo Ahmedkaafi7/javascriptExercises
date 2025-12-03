61ab5e0bd15b7dc93d2581ba29c2b56a282c010e
ahmedkaafi <ahmedkaafi.tech@gmail.com>
2025-12-03 10:56:10 +0300

    arrowFunctionExc5

---
+++ exercise5.js
@@ -0,0 +1,16 @@
+const addNumbers = (num1, num2) => {
+  console.log(num1 + num2);
+}
+addNumbers(5,10);
+addNumbers(21, 7);
+addNumbers(47, 10);
+addNumbers(500, 113);
+
+
+const add = (num1, num2) => {
+  return num1 + num2;
+}
+console.log(add(5,10));
+console.log(add(21, 7));
+console.log(add(47, 10));
+console.log(add(500, 113));
