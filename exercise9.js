17d09755c1bac7fc184b599e87be2ea1dd1dc077
ahmedkaafi <ahmedkaafi.tech@gmail.com>
2025-12-05 16:02:56 +0300

    objectMethodExc

---
+++ exercise9.js
@@ -0,0 +1,10 @@
+let car = {
+  make: "Toyota",
+  model: "Camry",
+  year: 2020,
+  color: "blue",
+  start : function() {
+    console.log(" The Car has started");
+  }
+};
+console.log(car.start());
