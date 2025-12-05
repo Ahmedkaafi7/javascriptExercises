aa7d48d050fad0036ece96c1063bf119ff6d4c21
ahmedkaafi <ahmedkaafi.tech@gmail.com>
2025-12-05 16:23:22 +0300

    combinationArray&Object

---
+++ exercise10.js
@@ -0,0 +1,7 @@
+let libery = [
+  { title: "The Great Gatsby", author: "F. Scott Fitzgerald", year: 1925 },
+   { title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960 },
+  { title: "1984", author: "George Orwell", year: 1949 }
+]
+console.log(libery[0].title);
+console.log(libery[1].author);
