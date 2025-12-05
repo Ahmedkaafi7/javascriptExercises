51123a024b063c9898f7f81a0b916d8db99604c0
ahmedkaafi <ahmedkaafi.tech@gmail.com>
2025-12-05 15:41:06 +0300

    objectExc

---
+++ exercise8.js
@@ -0,0 +1,15 @@
+let car = {
+  make: 'Toyota',
+  model: 'Camry',
+  year: 2020,
+  color: 'blue'
+}
+console.log(car.make);
+console.log(car.model);
+console.log(car.year);
+console.log(car.color);
+
+console.log(car['make']);
+console.log(car['model']);
+console.log(car['year']);
+console.log(car['color']);
