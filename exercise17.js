3b17fca50cdd1ce520006fef15d3afba74e1d786
ahmedkaafi <ahmedkaafi.tech@gmail.com>
2025-12-10 14:10:03 +0300

    elseifExc

---
+++ exercise17.js
@@ -0,0 +1,11 @@
+let temperature = -5;
+
+if (temperature <0) {
+console.log("It's very cold outside.");
+ } else if (temperature < 15){
+  console.log("It's cold outside.");
+ }else if (temperature < 25){ 
+  console.log("It's warm outside.");
+ }else{
+console.log("It's hot outside.");
+ };
