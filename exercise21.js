beb562db27e13e4c47a5f1d510d1f353af3c5c38
ahmedkaafi <ahmedkaafi.tech@gmail.com>
2025-12-12 15:13:30 +0300

    reduce&filterExc

---
+++ exercise21.js
@@ -0,0 +1,13 @@
+const numbers = [1, 2, 3, 4, 5];
+
+const multiplynumbers = numbers.reduce((total, numb) => total * numb, 1);
+
+console.log(multiplynumbers);
+
+
+//filter method
+
+
+// const scores = [45, 78, 88, 92, 56, 67];
+// const passedScores = scores.filter((score) => score >= 60);
+// console.log(passedScores);
