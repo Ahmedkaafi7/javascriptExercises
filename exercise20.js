3b1b4cb283ac6e9774ff3fffd47c0d9de888f4ae
ahmedkaafi <ahmedkaafi.tech@gmail.com>
2025-12-12 14:30:26 +0300

    mapExc

---
+++ exercise20.js
@@ -0,0 +1,13 @@
+const fruits = ["apple", "banana", "cherry"];
+
+const upperFruits = fruits.map((fruit) => fruit.length );
+console.log(upperFruits);
+
+
+// const students = [
+//     "Alice",
+//     "Bob", 
+//     "Charlie"
+// ];
+// const newstundent = students.map((student) => student.length);
+// console.log(newstundent);
