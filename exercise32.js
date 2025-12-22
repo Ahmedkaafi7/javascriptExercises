fb16f105961072279b835aaa27d50f03dfdfc250
ahmedkaafi <ahmedkaafi.tech@gmail.com>
2025-12-22 09:16:36 +0300

    DomQuareiesExc

---
+++ exercise32.js
@@ -0,0 +1,9 @@
+const title = document.querySelector('h1'); 
+
+console.log('title');
+
+const container = document.querySelector('#container');
+console.log('container');
+
+const paragraphs = document.querySelectorAll('.text');
+console.log('paragraphs')
